37c3a4b28852e4b50688d568973491a56d52269f
Abi-Fik10-15 <abresh127@gmail.com>
2026-07-21 21:51:25 -0900

    Normalize all line endings to LF

---
+++ my-saas-project/api/src/modules/users/users.service.ts
@@ -0,0 +1,51 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+@Injectable()
+export class UsersService {
+
+    async create(createUserDto: CreateUserDto){
+        return prisma.user.create({
+            data: createUserDto,
+        });
+    }
+
+    async findAll(){
+        return prisma.user.findMany({
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                createdAt: true,
+            },
+        });
+    }
+
+    async findone(id: number){
+        const user = await prisma.user.findUnique({
+            where: { id },
+            select: {
+                id: true,
+                name: true,
+                email: true,
+            },
+        });
+        if (!user) throw new NotFoundException('User not found');
+        return user;
+    }
+
+    async remove(id: number){
+        try{
+            return await prisma.user.delete({
+                where: { id }
+            });
+
+        } catch (error) {
+            throw new NotFoundException('User not found');
+
+        }
+    }
+}
