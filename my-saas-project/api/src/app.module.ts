dfa6b9e10c7f1ed994cdcd7489182286d6fa9304
Abi-Fik10-15 <abresh127@gmail.com>
2026-07-27 02:12:22 -0900

    Middleware globally

--- my-saas-project/api/src/app.module.ts
+++ my-saas-project/api/src/app.module.ts
@@ -1,12 +1,24 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
-
+import { TenantMiddleware } from './core/middleware/tenant.middleware';
 @Module({
   imports: [UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(TenantMiddleware)
+
+      .exclude(
+        { path: 'auth/login', method: RequestMethod.POST },
+        { path: 'auth/register', method: RequestMethod.POST}
+      )
+
+      .forRoutes('*');
+  }
+}
